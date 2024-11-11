033b3fd4152e4a1679677be803962ec4a19eccf8
prki <mar.pikna@gmail.com>
2024-11-11 23:27:20 +0100

    feat: generate id for callout match

--- server/frontend/wailsjs/go/models.ts
+++ server/frontend/wailsjs/go/models.ts
@@ -3,6 +3,7 @@ export namespace main {
 	export class SetupMatch {
 	    p1_name: string;
 	    p2_name: string;
+	    callout_id: string;
 	
 	    static createFrom(source: any = {}) {
 	        return new SetupMatch(source);
@@ -12,6 +13,7 @@ export namespace main {
 	        if ('string' === typeof source) source = JSON.parse(source);
 	        this.p1_name = source["p1_name"];
 	        this.p2_name = source["p2_name"];
+	        this.callout_id = source["callout_id"];
 	    }
 	}
 
