69b0f4ffb0257f59b0758f74e0d62adbf437f0cb
prki <mar.pikna@gmail.com>
2024-11-18 01:16:46 +0100

    feat: server dark bg for smaller eyeburn

--- server/frontend/src/views/Header.vue
+++ server/frontend/src/views/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row border bg-light mt-1">
+  <div class="row border mt-1">
     <div class="col">
       <h2 class="text-center">Modify header content</h2>
       <form>
@@ -17,6 +17,23 @@
           </div>
         </div>
       </form>
+    </div>
+  </div>
+
+  <div class="row border mt-4">
+    <div class="col">
+      <h2 class="text-center">Header content guide</h2>
+      <p>This lets the TO desk person modify the header of the renderer. It is to be used to call out the currently running tournament game.</p>
+      <p>Format which should be used for games is as follows:</p>
+      <ul>
+        <li>&lt;b&gt;TEKKEN 8 POOLS&lt;/b&gt; -- in case of a single running tournament</li>
+        <li>&lt;b&gt;TEKKEN 8 POOLS // GUILTY GEAR XRD TOP 4&lt;/b&gt; -- in case of concurrently running tournaments</li>
+      </ul>
+      <p>Meaning that the text is sent in bold tag, all text is uppercase. Names of games to be used are:</p>
+      <ul>
+        <li>Guilty Gear Xrd: GUILTY GEAR XRD</li>
+        <li>Street Fighter 6: STREET FIGHTER 6</li>
+      </ul>
     </div>
   </div>
 </template>
