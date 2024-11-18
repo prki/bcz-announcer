de2971f76d311094ba3144bd143c5f33d3635e7d
prki <mar.pikna@gmail.com>
2024-11-19 00:01:19 +0100

    feat: display logos in callout cards

--- renderer/frontend/src/components/CalloutCard.vue
+++ renderer/frontend/src/components/CalloutCard.vue
@@ -2,10 +2,12 @@
   <div class="col">
     <!--<div class="card" style="width: 18rem;">-->
     <!--<div class="card card-font bg-transparent">-->
-    <div class="card card-font bg-transparent" :class="{ border: status === 'dq', 'border-warning': status === 'dq' }" style="width: 18rem;">
-      <div class="card-header text-center">
+    <div class="card card-font bg-transparent mb-4" :class="{ border: status === 'dq', 'border-warning': status === 'dq' }" style="width: 18rem;">
+      <!--<div class="card-header text-center">
         {{ friendlyGameTitle(gameName) }}
       </div>
+      -->
+      <img :src="gameNameToLogoPath(gameName)" class="card-img-top">
       <div class="card-body bg-transparent" :class="status">
         <ul class="list-group list-group-flush text-center">
           <li class="list-group-item bg-transparent card-font">{{ p1Name }}</li>
@@ -33,6 +35,29 @@ defineExpose({
   changeStatus,
   id
 });
+
+// Function returning image path for a particular logo.
+// Simple switch/case since low number of options/static content
+function gameNameToLogoPath(gamename: string): string {
+  const basePath = "src/assets/images/"
+  switch(gamename) {
+    case "bbcf":
+      return basePath + "BBCF_logo.svg";
+    case "strive":
+      return basePath + "GGST_logo.svg";
+    case "xrd":
+      return basePath + "ggxrdrev2_logo.svg";
+    case "plusr":
+      return basePath + "GGAC+R_logo.svg";
+    case "sf6":
+      return basePath + "SF6_logo.svg";
+    case "tekken":
+      return basePath + "Tekken-8-logo_white.svg";
+    default:
+      return "";
+  }
+
+}
 
 // Simple switch/case since low number of options/static content
 function friendlyGameTitle(gamename: string): string {
@@ -68,5 +93,11 @@ function friendlyGameTitle(gamename: string): string {
 }
 .dq {
   background-color: yellow;
+}
+
+.card-img-top {
+  width: 100%;
+  height: 12vh;
+  object-fit: fill;
 }
 </style>
