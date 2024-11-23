13ce10de9b508c2be7fcce58ac4b623abbda579a
prki <mar.pikna@gmail.com>
2024-11-23 02:02:53 +0100

    feat: dynamically resize callout cards based on number

--- renderer/frontend/src/views/MatchCallout.vue
+++ renderer/frontend/src/views/MatchCallout.vue
@@ -5,10 +5,10 @@
     </div>
   </div>
   -->
-  <div class="row">
-    <div class="col">
-      <div class="row row-cols-md-4 justify-content-center align-items-center">
-        <CalloutCard v-for="(card, idx) in cards" :key="idx" ref="calloutCards" :state="'default'" :gameName="card.game_name" :p1Name="card.p1_name" :p2Name="card.p2_name" :id="card.callout_id" />
+  <div class="row h-100">
+    <div class="col h-100">
+      <div class="row row-cols-md-4 justify-content-center align-items-center h-100">
+        <CalloutCard v-for="(card, idx) in cards" :key="idx" ref="calloutCards" :state="'default'" :gameName="card.game_name" :p1Name="card.p1_name" :p2Name="card.p2_name" :id="card.callout_id" :cardSize="calloutCardSize" />
       </div>
     </div>
   </div>
@@ -16,19 +16,9 @@
 
 <script lang="ts" setup>
 import CalloutCard from '../components/CalloutCard.vue';
-import { reactive, ref, useTemplateRef } from 'vue';
+import { reactive, ref, useTemplateRef, computed } from 'vue';
 import * as wails from '../../wailsjs/runtime/runtime.js';
 import * as models from '../../wailsjs/go/models';
-
-/*const state = reactive({
-  A: {
-    p1Name: '',
-    p2Name: ''
-  }
-});
-*/
-
-//type CalloutCardType = InstanceType<typeof CalloutCard>;
 
 type cardJson = {
   game_name: string,
@@ -43,15 +33,48 @@ type cardUpdateJson = {
 };
 
 const calloutCardRef = useTemplateRef<typeof CalloutCard>("calloutCards");
+const cardCounter = reactive({counter: 0});
 
 const cards = ref<cardJson[]>([]);
+
+
+const calloutCardSize = computed(() => {
+  console.log("Computing cardsize, card counter:", cardCounter.counter);
+
+  if (cardCounter.counter === 0) {
+    return "cardsize-null";
+  }
+
+  if (cardCounter.counter <= 4) {
+    return "cardsize-large";
+  } else if (cardCounter.counter <= 8) {
+    return "cardsize-medium";
+  } else {
+    return "cardsize-small";
+  }
+});
+
+const addNewCard = (msgJson: cardJson) => {
+  cards.value.push(msgJson);
+  cardCounter.counter = cards.value.length;
+}
+
+const removeCard = (card_id: string) => {
+  const idx = cards.value.findIndex((card) => card.callout_id === card_id);
+  if (idx !== -1) {
+    cards.value.splice(idx, 1);
+    cardCounter.counter = cards.value.length;
+  } else {
+    console.log("[ERROR] No card with id:", card_id, "found.");
+  }
+}
 
 wails.EventsOn("callout/new", (msg: models.main.Message) => {
   console.log("Received callout new message:", msg);
   const msgJson: cardJson = JSON.parse(msg.message);  // This could be given a type since we know what the message is.
   console.log("Message as obj:", msgJson);
 
-  cards.value.push(msgJson);
+  addNewCard(msgJson);
 });
 
 wails.EventsOn("callout/update", (msg: models.main.Message) => {
@@ -76,13 +99,7 @@ wails.EventsOn("callout/update", (msg: models.main.Message) => {
 
 wails.EventsOn("callout/delete", (msg: models.main.Message) => {
   console.log("[INFO] Received callout delete message:", msg);
-  const idx = cards.value.findIndex((card) => card.callout_id === msg.message); 
-  if (idx !== -1) {
-    cards.value.splice(idx, 1);
-  }
-  else {
-    console.log("[ERROR] No card with id:", msg.message, "found!")
-  }
+  removeCard(msg.message);
 });
 </script>
 
